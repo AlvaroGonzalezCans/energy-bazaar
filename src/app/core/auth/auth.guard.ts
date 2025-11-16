4ab96d1808ce5d104f4e0aac12ddc2e393eefa81
AlvaroGonzalezCans <agonzalezmc@gmail.com>
2025-11-16 19:14:10 +0100

    feat: update aut guard when is not logged in goes to login, if it is goes to dashboard

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -2,9 +2,19 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
   const router = inject(Router);
 
-  return auth.isAuthenticated() ? true : router.parseUrl('/login');
+  const isAuthenticated = auth.isAuthenticated();
+
+  if (!isAuthenticated) {
+    return router.parseUrl('/login');
+  }
+
+  if (state.url === '/' || state.url === '') {
+    return router.parseUrl('/dashboard');
+  }
+
+  return true;
 };
